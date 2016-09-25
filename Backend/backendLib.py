eb24fbdaa2f07b33927ff69105147166988b91f8
mdelcourt <martin.delcourt@uclouvain.be>
2016-09-25 14:38:16 +0200

    merging branches

--- Backend/backendLib.py
+++ Backend/backendLib.py
@@ -1,6 +1,6 @@
 import sys
 import backendConfig as conf
-#import numpy optimisation 
+#import numpy optimisation
 
 
 if conf.useRoot:
@@ -8,14 +8,14 @@ if conf.useRoot:
     print "ROOT imported"
 else:
     print "ROOT not imported"
-    
+
 
 memDump=[]
 
 def getLumi(pix):
   #l = 3*((255-pix[0])*(255-pix[1])*(255-pix[2]))**1/3.
   l = 255.-((pix[0]+pix[1]+pix[2])/3.)
-  return l    
+  return l
 
 def isSquare(pix):
   if pix[0] == conf.squareCol[0] and pix[1] == conf.squareCol[1] and pix[2] == conf.squareCol[2]:
@@ -24,9 +24,9 @@ def isSquare(pix):
     return 0
 
 def getWesterns(pix,sx,sy):
-  
+
   lines = []
-  
+
   inLine = False
   (l0,l1,ly)=(0,0,0)
   for y in range(sy):
@@ -42,7 +42,7 @@ def getWesterns(pix,sx,sy):
           inLine=True
           l0=x
           ly=y
-  
+
   #print lines
   westerns=[]
   while len(lines)>0:
@@ -54,18 +54,17 @@ def getWesterns(pix,sx,sy):
         break
     lines.remove(candi)
   #print westerns
-    
+
   return(westerns)
 
 def getBkg(pix,w):
   (x0,x1,y0,y1) = w
 
   lumi = [0 for i in range(0,255)]
-  
   if conf.useRoot:
-    h = TH1I("h%s"%len(memDump),"h",256,0,256) 
-    memDump.append(h) 
-    
+    h = TH1I("h%s"%len(memDump),"h",256,0,256)
+    memDump.append(h)
+
   for x in range(x0,x1):
     for y in range(y0,y1):
       lumi[int(getLumi(pix[x,y]))]+=1
@@ -74,7 +73,7 @@ def getBkg(pix,w):
   if conf.useRoot:
     memDump.append(TCanvas())
     h.Draw()
-  
+
   mLumi = max(lumi)
   inMaximum=False
   bkg0 = 0
@@ -97,11 +96,11 @@ def getBkg(pix,w):
   sigma = (bkg1-bkg0)/2.355
   return(bkg,sigma)
   #print "Background = %s, [between %s and %s]"%(bkg,bkg0,bkg1)
-  
-  
-  
 
-def printWestern(pix,w,bkg=0): 
+
+
+
+def printWestern(pix,w,bkg=0):
   h_west = TH2I("hist%s"%len(memDump),"Western",w[1]-w[0],w[0],w[1],w[3]-w[2],w[2],w[3])
   for x in range(w[0],w[1]):
     for y in range(w[2],w[3]):
@@ -109,23 +108,24 @@ def printWestern(pix,w,bkg=0):
       if l<0:
         l=0
       h_west.Fill(x,y,l)
-  c = TCanvas() 
-  h_west.Draw("colz") 
-  memDump.append(h_west) 
+  c = TCanvas()
+  h_west.Draw("colz")
+  memDump.append(h_west)
   memDump.append(c)
 
 
 def getBkgProfile(pix,w,bkg=0,sBkg=0):
   (x0,x1,y0,y1) = w
   bkgProfile=[]
+  mask = [[0 for i in range(y0,y1)] for j in range(x0,x1)]
   if conf.useRoot:
     h_bkgProfile= TH1F("h%s"%len(memDump),"Bkg profile",x1-x0,x0,x1)
     memDump.append(h_bkgProfile)
-    h_west = TH2I("hist%s"%len(memDump),"Western",w[1]-w[0],w[0],w[1],w[3]-w[2],w[2],w[3]) 
-    memDump.append(h_west) 
+    h_west = TH2I("hist%s"%len(memDump),"Western",w[1]-w[0],w[0],w[1],w[3]-w[2],w[2],w[3])
+    memDump.append(h_west)
   Lmin = 255
   LMax = 0
-  for x in range(x0,x1):  
+  for x in range(x0,x1):
     nPixBkg= 0
     bkgTot = 0
     for y in range(y0,y1):
@@ -139,9 +139,11 @@ def getBkgProfile(pix,w,bkg=0,sBkg=0):
           h_west.Fill(x,y,l)
         bkgTot+=l
         nPixBkg+=1
+      else:
+	mask[x-x0][y-y0]=1
     if nPixBkg==0:
       nPixBkg=1
-    bkgProfile.append(bkgTot*1./nPixBkg) 
+    bkgProfile.append(bkgTot*1./nPixBkg)
     if conf.useRoot:
       h_bkgProfile.Fill(x,bkgTot*1./nPixBkg)
   if conf.useRoot:
@@ -150,9 +152,9 @@ def getBkgProfile(pix,w,bkg=0,sBkg=0):
     memDump.append(TCanvas())
     h_west.GetZaxis().SetRangeUser(Lmin-1,LMax+1)
     h_west.Draw("colz")
-      
-  
-  return(bkgProfile)
+
+
+  return(bkgProfile,mask)
 
 def getLumiProfile(pix,w,bkgPro):
   (x0,x1,y0,y1) = w
@@ -170,18 +172,18 @@ def getLumiProfile(pix,w,bkgPro):
   #memDump.append(TCanvas())
   #h_xLumiProfile.Draw()
   return(profile)
-    
-      
-      
-      
+
+
+
+
 def getTrends(lumi):
-  
+
   trends         = []
   t                 = [0,0,-1]
   prevLumi          = 0
   for x in range(len(lumi)):
     l=lumi[x]
-    
+
     if l > prevLumi + 1e-5:
       if t[2]<0: #Previous trend was decreasing
         t[1] = x-1
@@ -200,7 +202,7 @@ def getTrends(lumi):
   return (trends)
 
 def trendMerger(trends):
-  
+
   tMerged = []
   prevSign = 0
   prevTrend = [0,0,0]
@@ -219,7 +221,7 @@ def trendMerger(trends):
 def getPeaks(lumi,w):
   print w[0]
   (x0,x1,y0,y1) = w
-  
+
   trends = getTrends(lumi)
   #print trends
   stableTrends = []
@@ -227,16 +229,16 @@ def getPeaks(lumi,w):
     if t[1]-t[0]>=conf.nTrend:
       stableTrends.append(t)
   if conf.useRoot:
-    h_lumiProfile = TH1F("h%s"%len(memDump),"xLumiProfile",len(lumi),0,len(lumi)) 
+    h_lumiProfile = TH1F("h%s"%len(memDump),"xLumiProfile",len(lumi),0,len(lumi))
     memDump.append(h_lumiProfile)
   for x in range(len(lumi)):
     l=lumi[x]
     if conf.useRoot:
-      h_lumiProfile.Fill(x,l) 
+      h_lumiProfile.Fill(x,l)
   if conf.useRoot:
-    memDump.append(TCanvas()) 
-    h_lumiProfile.Draw() 
-  
+    memDump.append(TCanvas())
+    h_lumiProfile.Draw()
+
   mergedTrends = trendMerger(stableTrends)
   for t in mergedTrends:
     if conf.useRoot:
@@ -259,7 +261,7 @@ def getPeaks(lumi,w):
       h_line.SetLineWidth(1)
       memDump.append(h_line)
       h_line.Draw()
-  
+
   mins = []
   prevT = mergedTrends[0]
   for t in mergedTrends[1:]:
@@ -274,12 +276,12 @@ def getPeaks(lumi,w):
     print "ERROR, no minimums found..."
     return([])
   if conf.useRoot:
-    for m in mins: 
+    for m in mins:
       h_line = TLine(m,0,m,2000)
       h_line.SetLineWidth(1)
       h_line.SetLineStyle(3)
       memDump.append(h_line)
-      h_line.Draw() 
+      h_line.Draw()
 
   peaks = []
   peakStart = mins[0]
@@ -296,15 +298,15 @@ def getPeaks(lumi,w):
       peakStop = m
       peaks.append([peakStart,peakStop])
       peakStart= m
-  
+
   #Merging peaks if too small
   avSize=0
   for p in peaks:
     pSize = p[1]-p[0]
     avSize +=pSize*1./len(peaks)
-  
+
   mergedPeaks = []
-  
+
   for i in range(len(peaks)):
     p = peaks[i]
     pSize = p[1]-p[0]
@@ -322,12 +324,12 @@ def getPeaks(lumi,w):
           peaks[i+1][0]=p[0]
     else:
       mergedPeaks.append(p)
-  
+
   if len(mergedPeaks)<1:
     print "ERROR, no merged peaks..."
     return([])
   if conf.useRoot:
-    for p in mergedPeaks: 
+    for p in mergedPeaks:
       h_line = TLine(p[0],0,p[0],2000)
       h_line.SetLineWidth(3)
       memDump.append(h_line)
@@ -335,15 +337,15 @@ def getPeaks(lumi,w):
     h_line = TLine(p[1],0,p[1],2000)
     h_line.SetLineWidth(3)
     memDump.append(h_line)
-    h_line.Draw() 
+    h_line.Draw()
 
-    
+
   return([])
-  
-  
+
+
 def getOutIm(outIm, w, mask):
    (x0,x1,y0,y1) = w
-   
+
    for y in range(1,len (mask)-1):
       for x in range(1,len(mask[y])-1):
          if (mask[y][x] != mask[y][x+1]
@@ -351,17 +353,17 @@ def getOutIm(outIm, w, mask):
          or mask[y][x] != mask[y+1][x+1]):
             outIm.putpixel((x+x0,y+y0),(0,0,255,255))
    return outIm
-   
+
 def getOutIm2(outIm, w, mask):
    """ for testing if comprehension makes it faster. BTW It doesn't."""
    (x0,x1,y0,y1) = w
-   
+
    listPixels = [(x+x0,y+y0) for x in range(1,len(mask[0])-1) for y in range(1,len (mask)-1) if (mask[y][x] != mask[y][x+1]
          or mask[y][x] != mask[y+1][x]
          or mask[y][x] != mask[y+1][x+1])]
-         
+
    for (x,y) in listPixels:
       outIm.putpixel((x,y),(0,0,255,255))
    #print(len(listPixels))
    return outIm
-   
+
